5406cfacafcf21bf5e2cea039c5e4179c23a32b5
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-03-04 22:01:52 -0300

    DaoFactory and SellerDaoJDBC Class265

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +12,8 @@ public class Program {
 		Department obj = new Department(3,"Douglas");
 		
 		Seller seller = new Seller(4, "Douglas", "douglas@gmail.com", new Date(), 2000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println(seller);
 
