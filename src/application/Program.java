3d350cd2b2e5393b5eac16454475ce3db27e2cf8
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-03-05 19:13:44 -0300

    delete into SellerDaoJDBC class272

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +12,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = sellerDao.findById(26);
@@ -32,16 +32,27 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
-		System.out.println("\nTest4 SELLER insert");
+		/*System.out.println("\nTest4 SELLER insert");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println("Inserted! New id = " + newSeller.getId());*/
 		
-		System.out.println("\nTest4 SELLER update");
+		System.out.println("\nTest5 SELLER update");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\nTest6 SELLER delete");
+		System.out.println("Enter id ofr delete test: ");
+		int x = 1;
+		while(x==1) {
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("delete completed");
+		
+		}
+		sc.close();
 	}
 
 }
