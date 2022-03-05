f9d877ab9dfef011c67f62f9c055570752afe4f3
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-03-05 15:28:31 -0300

    findAll into SellerDaoJDBC class269

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,12 +15,19 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = sellerDao.findById(26);
-		System.out.println("Test1");
+		System.out.println("Test1 find by Id");
 		System.out.println(seller);
-		System.out.println("\nTest2");
+		System.out.println("\nTest2 find by department");
 		sellerDao = DaoFactory.createSellerDao();
 		Department department = new Department(4, null);
 		List<Seller>list=sellerDao.findByDepartment(department);
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
+		System.out.println("\nTest3 find all");
+		sellerDao = DaoFactory.createSellerDao();
+		
+		list=sellerDao.findAll();
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -102,8 +102,40 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+			PreparedStatement st = null;
+		 	ResultSet rs = null;
+		 	try {
+		 		st=conn.prepareStatement(
+		 				"SELECT seller.*,department.Name as DepName "
+		 				+ "FROM seller INNER JOIN department "
+		 				+ "ON seller.DepartmentId = department.Id "
+		 				+ "ORDER bY Id");
+
+		 		rs = st.executeQuery();
+		 		List<Seller> list = new ArrayList<>();
+		 		Map<Integer, Department>map = new HashMap<>();
+		 		
+		 		while(rs.next()) {
+		 
+		 			Department dep = map.get(rs.getInt("DepartmentId"));
+		 			if(dep== null) {
+		 				 dep = instanciateDepartment(rs);
+		 				 map.put(rs.getInt("DepartmentId"), dep);
+		 			}
+		 			
+		 			
+		 			Seller obj = instatiateSeller(rs, dep);
+		 			list.add(obj);
+		 		}
+		 		return list;
+		 	}
+		 	catch(SQLException e) {
+		 		throw new DbException(e.getMessage());
+		 	}
+		 	finally {
+		 		DB.closeStatement(st);
+		 		DB.closeResultSet(rs);
+		 	}
 	}
 
 	@Override
@@ -123,23 +155,14 @@ public class SellerDaoJDBC implements SellerDao{
 	 		Map<Integer, Department>map = new HashMap<>();
 	 		
 	 		while(rs.next()) {
-	 			/*Department dep = new Department();
-	 			dep.setId(rs.getInt("DepartmentId"));
-	 			dep.setName(rs.getString("depName"));*/
+	 
 	 			Department dep = map.get(rs.getInt("DepartmentId"));
 	 			if(dep== null) {
 	 				 dep = instanciateDepartment(rs);
 	 				 map.put(rs.getInt("DepartmentId"), dep);
 	 			}
 	 			
-	 			/*Seller obj = new Seller();
-	 			obj.setId(rs.getInt("Id"));
-	 			obj.setName(rs.getString("Name"));
-	 			obj.setEmail(rs.getString("Email"));
-	 			obj.setBirthDate(rs.getDate("BirthDate"));
-	 			obj.setBaseSalary(rs.getDouble("BaseSalary"));
-	 			obj.setDepartament(dep);
-	 			return obj;*/
+	 			
 	 			Seller obj = instatiateSeller(rs, dep);
 	 			list.add(obj);
 	 		}
